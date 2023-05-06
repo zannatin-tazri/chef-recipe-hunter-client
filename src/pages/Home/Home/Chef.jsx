0faacfc6d46ebd3e2191798916631eb0ab5a8283
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-06 21:37:41 +0600

    chefs info added

--- src/pages/Home/Home/Chef.jsx
+++ src/pages/Home/Home/Chef.jsx
@@ -2,30 +2,39 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { Button } from 'react-bootstrap';
+
 
 const Chef = (props) => {
     console.log(props.chef);
     const {name,id,likes,picture,numberOfRecipes,yearsOfExperience}=props.chef;
     
     return (
-        <div>
-           <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 1 }).map((_, idx) => (
+        <div className='m-5 '> 
+         <Row lg={1} md={2} className="g-4">
+      {Array.from({ length: 1 }).map((_3, id) => (
         <Col>
-          <Card>
-            <Card.Img variant="top" src={picture} />
+          <Card className='p-5'>
+            <Card.Img className='rounded' variant="top" src={picture} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              <Card.Title>{name}</Card.Title>
+              <Card.Text className='d-flex justify-content-between'>
+                <h3>Recipes : {numberOfRecipes} </h3> 
+                <h3><FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon> {likes} Likes</h3>
+              </Card.Text>
+              <Card.Text  className='d-flex justify-content-between'>
+                <h5>{yearsOfExperience} Years Experience</h5>
+                <Button variant="dark" className="text-center">View Details</Button>
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
       ))}
     </Row>
+
+      
         </div>
     );
 };
--- src/pages/Home/Home/Chefs.jsx
+++ src/pages/Home/Home/Chefs.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Chef from './Chef';
 
+
 const Chefs = () => {
 
     const [chefs, setChefs]=useState([]);
@@ -14,9 +15,12 @@ const Chefs = () => {
     },[])
     return (
         <div>
-            <h2>Our Chefs</h2>
+            <hr />
+            <h1 className='text-center fw-bold mt-5'>Amazing Chefs of Our Restaurant</h1>
+            <small className='text-break text-center fst-italic fw-lighter'>Dim Sum Dynasty restaurant chefs include the Head Chef or Executive Chef, Dim Sum Chef, and Wok Chef. The Head Chef oversees the kitchen, while Dim Sum Chefs specialize in small, bite-sized dishes, and Wok Chefs excel in high-heat cooking techniques using a wok.
+            Dim Sum Dynasty restaurant chefs include the Head Chef or Executive Chef, Dim Sum Chef, and Wok Chef. </small>
 
-            <Container>
+            <Container className='chefs-container'>
                  {
                      chefs.map(chef=><Chef
                      key={chef.id}
